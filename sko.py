aac3fd726af086a907dc7ebce855808fc794bf86
vcasado <vcasado@cloudbees.com>
2020-02-20 19:28:47 +0100

    Adding rollput env key

--- sko.py
+++ sko.py
@@ -19,7 +19,7 @@ flags = Flags()
 Rox.register("default", flags)
 
 # Setup the Rollout environment key
-cancel_event = Rox.setup("<rollout_env_key>").result();
+cancel_event = Rox.setup("5e4ecf5c80438cebd6ae8fbe").result();
 
 # Boolean flag example
 print('enableTutorial is %s' % flags.enableTutorial.is_enabled())
